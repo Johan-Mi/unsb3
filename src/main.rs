e5cc55d7f46765b8dfe719d4ab690c7dcfe0758c
Johan-Mi <johanmi@protonmail.com>
2022-03-09 14:55:42 +0100

    Read the project as a zip file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::vm::VM;
-use std::{fs::File, io::BufReader};
+use std::fs::File;
 
 mod deser;
 mod expr;
@@ -9,7 +9,8 @@ mod statement;
 mod vm;
 
 fn main() {
-    let path = "project.json";
+    let path = "project.sb3";
+
     let file = match File::open(path) {
         Ok(file) => file,
         Err(err) => {
@@ -17,8 +18,24 @@ fn main() {
             return;
         }
     };
-    let reader = BufReader::new(file);
-    let vm: VM = match serde_json::from_reader(reader) {
+
+    let mut archive = match zip::ZipArchive::new(file) {
+        Ok(zip) => zip,
+        Err(err) => {
+            eprintln!("Zip error: {err}");
+            return;
+        }
+    };
+
+    let project_json = match archive.by_name("project.json") {
+        Ok(zip) => zip,
+        Err(err) => {
+            eprintln!("Zip error: {err}");
+            return;
+        }
+    };
+
+    let vm: VM = match serde_json::from_reader(project_json) {
         Ok(vm) => vm,
         Err(err) => {
             eprintln!("Deserialization error: {err}");
